a3f1d98825c4567016a10aae66f0632c7bd99878
rvcoutinho <me@rvcoutinho.com>
2021-08-09 14:59:16 -0300

    - Fixing script variables;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/repository/script/psql/psql_script.sh
+++ src/main/infrastructure/repository/script/psql/psql_script.sh
@@ -4,5 +4,5 @@
 # Executes the script.
 echo "Starting script"
 COMMAND="$@"
-echo ${USER_PASSWORD} | exec psql -c "${COMMAND}" -h ${DATABASE_HOST} -d ${POSTGRES_DEFAULT_DATABASE} -U ${USER_NAME} -W
+echo ${POSTGRES_DEFAULT_PASSWORD} | exec psql -c "${COMMAND}" -h ${DATABASE_HOST} -d ${POSTGRES_DEFAULT_DATABASE} -U ${POSTGRES_DEFAULT_USER} -W
 echo "Ending script"

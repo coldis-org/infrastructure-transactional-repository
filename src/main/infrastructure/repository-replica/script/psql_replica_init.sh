249787e822ed3f4c96f1e419e67e9d01b5bb183c
rvcoutinho <me@rvcoutinho.com>
2023-06-04 08:29:01 -0300

    - Fixing;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/repository-replica/script/psql_replica_init.sh
+++ src/main/infrastructure/repository-replica/script/psql_replica_init.sh
@@ -12,13 +12,31 @@ REPLICATION_LOCK_FILE=${PGDATA}/replication_configured.lock
 trap - INT TERM
 
 # If the replication has not been configured yet.
-if [ ! -f ${REPLICATION_LOCK_FILE} ]; then
+if [ "${FORCE_BACKUP}" = "true" ]
+then
+	echo "Removing replication file"
+	rm ${REPLICATION_LOCK_FILE}
+fi
+
+if [ ! -f ${REPLICATION_LOCK_FILE} ] 
+then
 
 	# Cleans data.
 	rm -rf ${PGDATA}/*
+
+	# Sets the slot.
+	SLOT_PARAM=
+	if [ ! -z "${REPLICATION_SLOT_NAME}" ]
+	then
+		SLOT_PARAM="--slot=${REPLICATION_SLOT_NAME}"
+	fi
+
+	BACKUP_PARAMS="""--host=${MASTER_ENDPOINT} --port=${MASTER_PORT} --pgdata=${PGDATA} --username=${POSTGRES_REPLICATOR_USER}\
+			--format=tar --wal-method=stream --checkpoint=fast --progress --verbose --write-recovery-conf ${SLOT_PARAM}"""
 	# Starts replication streaming.
-	echo ${POSTGRES_REPLICATOR_PASSWORD} | pg_basebackup -h ${MASTER_ENDPOINT} -p ${MASTER_PORT} -D ${PGDATA} -PRv -U ${POSTGRES_REPLICATOR_USER} -X stream --checkpoint=fast
-	# Stes that replication has been configured.
+	(echo ${POSTGRES_REPLICATOR_PASSWORD} | pg_basebackup ${BACKUP_PARAMS} --create-slot) || \
+	(echo ${POSTGRES_REPLICATOR_PASSWORD} | pg_basebackup ${BACKUP_PARAMS})
+	# Sets that replication has been configured.
 	touch ${REPLICATION_LOCK_FILE}
 
 fi

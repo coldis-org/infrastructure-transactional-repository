18008bf4e4166dbc82edd2324e6296f25542b9ba
rvcoutinho <me@rvcoutinho.com>
2025-08-15 09:11:42 -0300

    - Upgrading; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/repository/script/psql/psql_configure.sh
+++ src/main/infrastructure/repository/script/psql/psql_configure.sh
@@ -26,6 +26,10 @@ do
 	sleep 1
 done
 
+# Configures timeout for admin user.
+${DEBUG} && echo "Setting admin user timeout."
+PGPASSWORD=${POSTGRES_ADMIN_PASSWORD:=postgres} psql -c "ALTER ROLE CURRENT_ROLE SET statement_timeout = ${ADMIN_STATEMENT_TIMEOUT:=0};" -U ${POSTGRES_ADMIN_USER:=postgres} || true
+PGPASSWORD=${POSTGRES_ADMIN_PASSWORD:=postgres} psql -c "ALTER ROLE CURRENT_ROLE SET lock_timeout = ${ADMIN_LOCK_TIMEOUT:=0};" -U ${POSTGRES_ADMIN_USER:=postgres} || true
 
 # If the user has not been (and should) configured yet.
 ${DEBUG} && echo "POSTGRES_DEFAULT_USER=${POSTGRES_DEFAULT_USER}"

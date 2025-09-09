62e7a3985df154bc27688b930680df1a7f5cec7f
Bruno Afonso <bruno.afonso@supersim.com.br>
2025-09-09 13:24:21 -0300

    Add checks

--- src/main/infrastructure/repository/script/psql/psql_copy_seq.sh
+++ src/main/infrastructure/repository/script/psql/psql_copy_seq.sh
@@ -35,30 +35,32 @@ while :; do
   shift
 done
 
-if [ "$COPY" = "true" ] && [ "$APPLY" = "true" ]; then
-  echo "For safety use first copy and then apply"
-  exit 1
+if [ $UPGRADE_SERVICE = "true" ]; then
+
+  if [ "$COPY" = "true" ] && [ "$APPLY" = "true" ]; then
+    echo "For safety use first copy and then apply"
+    exit 1
+  fi
+
+  if [ "$COPY" = "true" ];  then
+
+      echo "GET SEQUENCE AND INCREASE BY: ${INC_VAL}"
+      PGPASSWORD=${POSTGRES_ADMIN_PASSWORD:=postgres} psql -h "${MASTER_ENDPOINT}" -p "${MASTER_PORT}" -U ${POSTGRES_ADMIN_USER} -d ${POSTGRES_DEFAULT_DATABASE} -At -c "
+      SELECT format(
+          'SELECT setval(%L, %s, true);',
+          format('%I.%I', schemaname, sequencename),
+          COALESCE(last_value, start_value) + increment_by + cache_size + ${INC_VAL}
+      )
+      FROM pg_sequences
+      ORDER BY 1
+      " > ${SEQ_FILE}
+
+      echo "SEQUENCE TO APPLY..."
+      cat ${SEQ_FILE}
+  fi
+
+  if [ "$APPLY" = "true" ];  then
+      echo "APPLYING SEQUENCES..."
+      PGPASSWORD="${POSTGRES_ADMIN_PASSWORD:=postgres}" psql -U "$POSTGRES_ADMIN_USER" -d "$POSTGRES_DEFAULT_DATABASE" -f ${SEQ_FILE}
+  fi
 fi
-
-if [ "$COPY" = "true" ];  then
-
-    echo "GET SEQUENCE AND INCREASE BY: ${INC_VAL}"
-    PGPASSWORD=${POSTGRES_ADMIN_PASSWORD:=postgres} psql -h "${MASTER_ENDPOINT}" -p "${MASTER_PORT}" -U ${POSTGRES_ADMIN_USER} -d ${POSTGRES_DEFAULT_DATABASE} -At -c "
-    SELECT format(
-        'SELECT setval(%L, %s, true);',
-        format('%I.%I', schemaname, sequencename),
-        COALESCE(last_value, start_value) + increment_by + cache_size + ${INC_VAL}
-    )
-    FROM pg_sequences
-    ORDER BY 1
-    " > ${SEQ_FILE}
-
-    echo "SEQUENCE TO APPLY..."
-    cat ${SEQ_FILE}
-fi
-
-if [ "$APPLY" = "true" ];  then
-    echo "APPLYING SEQUENCES..."
-    PGPASSWORD="${POSTGRES_ADMIN_PASSWORD:=postgres}" psql -U "$POSTGRES_ADMIN_USER" -d "$POSTGRES_DEFAULT_DATABASE" -f ${SEQ_FILE}
-fi
-
